afa2aeb6bfde4f9d6ac12b6879c6edda61c06d87
Martin Lundberg <martin_o_lundberg@hotmail.com>
2023-11-02 19:04:59 +0100

    use json data with patterns

--- lib/data.dart
+++ lib/data.dart
@@ -2,19 +2,21 @@ import 'dart:convert';
 
 class Document {
   final Map<String, Object?> _json;
+
   Document() : _json = jsonDecode(documentJson);
 
   (String, {DateTime modified}) get metadata {
-    if (_json.containsKey('metadata')) {
-      final metadataJson = _json['metadata'];
-      if (metadataJson is Map) {
-        final title = metadataJson['title'] as String;
-        final localModified =
-        DateTime.parse(metadataJson['modified'] as String);
-        return (title, modified: localModified);
-      }
+    if (_json
+        case {
+          'metadata': {
+            'title': String title,
+            'modified': String localModified,
+          }
+        }) {
+      return (title, modified: DateTime.parse(localModified));
+    } else {
+      throw const FormatException('Unexpected JSON');
     }
-    throw const FormatException('Unexpected JSON');
   }
 }
 
